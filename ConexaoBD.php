23f52c14a2bd03436d214348fe0a2466694e2c12
ericbrambilaifc <ericbrambilaifc@gmail.com>
2025-06-09 16:29:37 -0300

    save

---
+++ ConexaoBD.php
@@ -0,0 +1,11 @@
+<?php 
+
+class ConexaoBD{
+    public static function conectar():PDO {
+
+        $conexao = new PDO("pgsql:host=localhost;dbname=terceiro_a","postgres", "postgres");
+        return $conexao;
+    }
+}
+
+$conexao = ConexaoBD::conectar();
